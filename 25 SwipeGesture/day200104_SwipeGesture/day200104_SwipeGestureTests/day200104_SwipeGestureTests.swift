cdfdd11d7e71a1774fbd5cc16d19ba29708916b6
moonibot <59480529+moonibot@users.noreply.github.com>
2020-01-04 19:16:45 +0900

    create project

---
+++ 25 SwipeGesture/day200104_SwipeGesture/day200104_SwipeGestureTests/day200104_SwipeGestureTests.swift
@@ -0,0 +1,34 @@
+//
+//  day200104_SwipeGestureTests.swift
+//  day200104_SwipeGestureTests
+//
+//  Created by 무니 on 2020/01/04.
+//  Copyright © 2020 com.mooni. All rights reserved.
+//
+
+import XCTest
+@testable import day200104_SwipeGesture
+
+class day200104_SwipeGestureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
