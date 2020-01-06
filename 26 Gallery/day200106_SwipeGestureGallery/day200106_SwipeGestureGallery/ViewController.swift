a86e32f46971d88dba2984c7b2ea246808a9dd0d
moonibot <59480529+moonibot@users.noreply.github.com>
2020-01-06 22:44:28 +0900

    Comment
    주석 입력

--- 26 Gallery/day200106_SwipeGestureGallery/day200106_SwipeGestureGallery/ViewController.swift
+++ 26 Gallery/day200106_SwipeGestureGallery/day200106_SwipeGestureGallery/ViewController.swift
@@ -23,10 +23,13 @@ class ViewController: UIViewController {
         
         pageControl.numberOfPages = images.count
         pageControl.currentPage = 0
+        // 페이지 표시 색상
         pageControl.pageIndicatorTintColor = UIColor.lightGray
+        // 현재 페이지 표시 색상
         pageControl.currentPageIndicatorTintColor = UIColor.black
         imgView.image = UIImage(named: images[0])
         
+        // 한 손가락 스와이프 제스쳐 등록(좌, 우)
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
         self.view.addGestureRecognizer(swipeLeft)
@@ -35,6 +38,7 @@ class ViewController: UIViewController {
         swipeRight.direction = UISwipeGestureRecognizer.Direction.right
         self.view.addGestureRecognizer(swipeRight)
         
+        // 두 손가락 스와이프 제스쳐 등록(좌, 우)
         let swipeLeftMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
         swipeLeftMulti.direction = UISwipeGestureRecognizer.Direction.left
         swipeLeftMulti.numberOfTouchesRequired = numOfTouchs
@@ -53,6 +57,8 @@ class ViewController: UIViewController {
         // 만일 제스쳐가 있다면
         if let swipeGesture = gesture as? UISwipeGestureRecognizer{
             
+            // 발생한 이벤트가 각 방향의 스와이프 이벤트라면
+            // pageControl이 가르키는 현재 페이지에 해당하는 이미지를 imageView에 할당
             switch swipeGesture.direction {
                 case UISwipeGestureRecognizer.Direction.left :
                     pageControl.currentPage -= 1
@@ -71,7 +77,6 @@ class ViewController: UIViewController {
     // 두 손가락 스와이프 제스쳐를 행했을 때 실행할 액션 메서드
     @objc func respondToSwipeGestureMulti(_ gesture: UIGestureRecognizer) {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-
             switch swipeGesture.direction {
                case UISwipeGestureRecognizer.Direction.left:
                     pageControl.currentPage -= 1
@@ -86,8 +91,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func pageChanged(_ sender: UIPageControl) {
-        print("page Changed")
-        
+        // print("page Changed")
         imgView.image = UIImage(named: images[pageControl.currentPage])
     }
 }
