1a0b4ac2a639d5008b77e27572baef64163157b2
moonibot <59480529+moonibot@users.noreply.github.com>
2020-01-07 18:28:14 +0900

    Comment
    텍스트 핀치 제스처 주석 입력

--- 27 PinchGesture/day200107_PinchGesture/day200107_PinchGesture/ViewController.swift
+++ 27 PinchGesture/day200107_PinchGesture/day200107_PinchGesture/ViewController.swift
@@ -11,20 +11,24 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var txtPinch: UILabel!
     
-    var initialFontSize: CGFloat!
+    var initialFontSize: CGFloat! // 글자 크기를 저장하기 위한 변수
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // UIPinchGestureRecognizer 클래스 상수 pinch 선언
         let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
-        self.view.addGestureRecognizer(pinch)
+        self.view.addGestureRecognizer(pinch) // 핀치 제스처 등록
     }
     
     @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        // 핀치 제스처 상태 확인
         if (pinch.state == UIPinchGestureRecognizer.State.began) {
+            // 시작 상태이면 현재 글자 크기를 저장
             initialFontSize = txtPinch.font.pointSize
         } else {
+            // 시작 상태가 아니면 텍스트의 글자 크기를 변경
             txtPinch.font = txtPinch.font.withSize(initialFontSize * pinch.scale)
         }
     }
