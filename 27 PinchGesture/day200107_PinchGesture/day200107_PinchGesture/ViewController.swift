f52882f423be4bfb10ea9db3f7d0a2a90cd73ba0
moonibot <59480529+moonibot@users.noreply.github.com>
2020-01-07 18:04:55 +0900

    Create Pinch Gesture project
    핀치 제스쳐(Pinch Gesture) 프로젝트 생성

---
+++ 27 PinchGesture/day200107_PinchGesture/day200107_PinchGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day200107_PinchGesture
+//
+//  Created by 무니 on 2020/01/07.
+//  Copyright © 2020 com.mooni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 27 PinchGesture/day200107_PinchGesture/day200107_PinchGestureTests/day200107_PinchGestureTests.swift
@@ -0,0 +1,34 @@
+//
+//  day200107_PinchGestureTests.swift
+//  day200107_PinchGestureTests
+//
+//  Created by 무니 on 2020/01/07.
+//  Copyright © 2020 com.mooni. All rights reserved.
+//
+
+import XCTest
+@testable import day200107_PinchGesture
+
+class day200107_PinchGestureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
