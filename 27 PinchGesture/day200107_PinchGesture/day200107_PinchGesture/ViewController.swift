4f1794b4dd3dcf7c1f29f7f25dc2b1447c9365c3
moonibot <59480529+moonibot@users.noreply.github.com>
2020-01-07 19:46:33 +0900

    Comment
    이미지 핀치 제스처 주석 입력

--- 27 PinchGesture/day200107_PinchGesture/day200107_PinchGesture/ViewController.swift
+++ 27 PinchGesture/day200107_PinchGesture/day200107_PinchGesture/ViewController.swift
@@ -24,7 +24,9 @@ class ViewController: UIViewController {
     }
     
     @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        // 이미지를 스케일에 맞게 변환
         imgPinch.transform = imgPinch.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+        // 다음 변환을 위해 핀치의 스케일 속성을 1로 설정
         pinch.scale = 1
     }
 
