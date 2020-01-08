c8b5734fcc6425b6ecd588a65bd4bcc457b10063
moonibot <59480529+moonibot@users.noreply.github.com>
2020-01-08 15:47:36 +0900

    Create PinchGesture-Example project
    핀치 제스처(Pinch Gesture) 예제 프로젝트 생성

---
+++ 28 PinchGesture-Example/day200108_PinchGestureGallery/day200108_PinchGestureGalleryTests/day200108_PinchGestureGalleryTests.swift
@@ -0,0 +1,34 @@
+//
+//  day200108_PinchGestureGalleryTests.swift
+//  day200108_PinchGestureGalleryTests
+//
+//  Created by 무니 on 2020/01/08.
+//  Copyright © 2020 com.mooni. All rights reserved.
+//
+
+import XCTest
+@testable import day200108_PinchGestureGallery
+
+class day200108_PinchGestureGalleryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
