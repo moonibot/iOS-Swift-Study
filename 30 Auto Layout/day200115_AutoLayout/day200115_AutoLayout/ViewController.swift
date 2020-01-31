9a9c01ac3b6d47be56857dd7fce60eaa4f4c47e3
moonibot <59480529+moonibot@users.noreply.github.com>
2020-01-31 20:54:08 +0900

    SwiftUI Calculator - Master Stacking
    SwiftUI Calculator - stacking library 2차원 격자 버튼 만드는 방법

---
+++ 30 Auto Layout/day200115_AutoLayout/day200115_AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day200115_AutoLayout
+//
+//  Created by 무니 on 2020/01/15.
+//  Copyright © 2020 com.mooni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 30 Auto Layout/day200115_AutoLayout/day200115_AutoLayoutTests/day200115_AutoLayoutTests.swift
@@ -0,0 +1,34 @@
+//
+//  day200115_AutoLayoutTests.swift
+//  day200115_AutoLayoutTests
+//
+//  Created by 무니 on 2020/01/15.
+//  Copyright © 2020 com.mooni. All rights reserved.
+//
+
+import XCTest
+@testable import day200115_AutoLayout
+
+class day200115_AutoLayoutTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
