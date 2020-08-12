000d5bf0428eaf3ddf21de5502174c74de2ccbbc
moonibot <59480529+moonibot@users.noreply.github.com>
2020-08-12 22:32:36 +0900

    collection study
    컬렉션 여러 함수 써보기

--- Example/day200812_CollectionView/day200812_CollectionView/ViewController.swift
+++ Example/day200812_CollectionView/day200812_CollectionView/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     var collectionItems = [String]()
     
@@ -46,8 +46,59 @@ class ViewController: UICollectionViewController {
         
         //  Configure the Cell
         cell.collectionviewTest.text = collectionItems[indexPath.row]
+        
+        
+        var bgColor = UIColor.init()
+        if indexPath.row % 2 == 0 {
+            bgColor = UIColor.green
+        } else {
+            bgColor = UIColor.red
+        }
+        
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+        cell.backgroundColor = bgColor
+        
         return cell
     }
+    
+    // 사이즈
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewCellWithd = collectionView.frame.width / 3 - 1
+           
+        return CGSize(width: collectionViewCellWithd, height: collectionViewCellWithd)
+
+    }
+
+       // 위아래 라인 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+
+        return 1
+
+    }
+
+    // 옆 라인 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+           
+        return 1
+    }
+  
+    // change background color when user touches cell
+    /*
+    override func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.red
+    }
+    */
+    
+    // change background color back when user releases touch
+    /*
+    override func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.cyan
+    }
+    */
 
 }
 
